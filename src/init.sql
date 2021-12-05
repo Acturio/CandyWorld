eab5973956efe5f118a61db0ce177efd26917629
Acturio <act.arturo.b@ciencias.unam.mx>
2021-12-05 14:01:51 -0600

    se actualiza proceso docker compose para ingesta de datos

--- src/init.sql
+++ src/init.sql
@@ -1,8 +1,18 @@
 CREATE DATABASE health;
 \connect health
 CREATE TABLE IF NOT EXISTS heart (
-    id smallint not null, 
-    title character varying(200) not null, 
-    description character varying (1000) not null
+    Age            int,      
+    Sex            varchar(50),  
+    ChestPainType  varchar(50),
+    RestingBP      numeric,  
+    Cholesterol    numeric,
+    FastingBS      numeric, 
+    RestingECG     varchar(50),
+    MaxHR          numeric,
+    ExerciseAngina varchar(50),
+    Oldpeak        numeric, 
+    ST_Slope       varchar(50),
+    HeartDisease   varchar(50)
 );
-\copy heart (id, title, description) FROM '/var/lib/postgresql/pg_data/' CSV HEADER DELIMITER ',' ;
+\copy heart (Age, Sex, ChestPainType, RestingBP, Cholesterol, FastingBS, RestingECG, MaxHR, ExerciseAngina, Oldpeak, ST_Slope, HeartDisease) FROM '/var/lib/postgresql/pg_data/' CSV HEADER DELIMITER ',' ;
+
